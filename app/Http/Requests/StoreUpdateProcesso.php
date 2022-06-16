3cd31c72b73a311bb5ddfe9457384506ca605335
Velito Gujamo <vgujamo@gmail.com>
2022-06-16 09:36:06 +0200

    Estavel: Processos e dropdown

--- app/Http/Requests/StoreUpdateProcesso.php
+++ app/Http/Requests/StoreUpdateProcesso.php
@@ -26,11 +26,11 @@ class StoreUpdateProcesso extends FormRequest {
         return [
             'num_processo' => 'required|min:3|max:50',
             'data_entrada' => 'required|min:3|max:50',
-            'seccao' => 'required|min:3|max:50',
-            'subseccao' => 'required|min:3|max:50',
-            'especie' => 'required|min:3|max:50',
+            'seccao_id' => 'required|min:1|max:2',
+            'subseccao_id' => 'required|min:1|max:2',
+            'especie_id' => 'required|min:1|max:2',
             'requerrente' => 'required|min:3|max:50',
-            'recorrido' => 'required|min:3|max:50',
+            'recorrido' => 'nullable|min:3|max:50',
             'contaparte' => 'required|min:3|max:50',
             'objecto' => 'required|min:3|max:100',
             'anexo' => 'nullable|file',
@@ -41,16 +41,17 @@ class StoreUpdateProcesso extends FormRequest {
     public function messages() {
        return [
         'num_processo.required' => 'O campo de processso é obrigatorio',
+        'num_processo.unique' => 'O numero de processo ja existe',
         'data_entrada.required' => 'O campo data de entrada é obrigatorio',
-        'seccao.required' => 'O campo secção é obrigatorio',
-        'subseccao.required' => 'O campo subsecção é obrigatorio',
-        'especie.required' => 'O campo especie é  obrigatorio',
+        'seccao_id.required' => 'O campo secção é obrigatorio',
+        'subseccao_id.required' => 'O campo subsecção é obrigatorio',
+        'especie_id.required' => 'O campo especie é  obrigatorio',
         'requerrente.required' => 'O campo requerente é obrigatorio',
-        'recorrido.required' => 'O campo ecorrido é obrigatorio',
+        'recorrido.nullable' => 'O campo ecorrido é opcional',
         'contaparte.required' => 'O campo contrapate é obrigatorio',
         'objecto.required' => 'O campo objecto é obrigatorio',
-        'anexo.nullable' => 'O campo anexo é obrigatorio',
-        'descricao.nullable' => 'O campo é obrigatorio'
+        'anexo.nullable' => 'O campo anexo é opcional',
+        'descricao.nullable' => 'O campo é opcional',
         ];
     }
 
